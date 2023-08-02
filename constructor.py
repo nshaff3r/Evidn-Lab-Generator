213735ef93e1195b04a1584fbb061447e34abf8c
Nolan Shaffer <nshaff3r@nolans-air.lan>
2023-08-02 11:22:49 -0400

    fixing bug

---
+++ constructor.py
@@ -0,0 +1,24 @@
+class Lab:
+    def __init__(self, name, baseline_avg, week_avg, months_avg, energy_saved, miles, phones, homes, alerts, building):
+        self.name = name
+        self.baseline_avg = baseline_avg
+        self.week_avg = week_avg
+        self.months_avg = months_avg  # Should be a dict of months and avgs
+        self.energy_saved = energy_saved
+        self.miles = miles
+        self.phones = phones
+        self.homes = homes
+        self.alerts = alerts
+        self.building = building
+
+
+class Building:
+    def __init__(self, name, shorthand, average, week):
+        self.name = name
+        self.shorthand = shorthand
+        self.average = average
+        self.labs = []
+        self.week = week
+
+    def add_lab(self, lab):
+        self.labs.append(lab)
